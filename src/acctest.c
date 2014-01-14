728793ae2bd8b3b4522982aeb68e830284d3e303
sappling <sajakarta@appling.org>
2014-01-13 21:05:03 -0500

    initial version

---
+++ src/acctest.c
@@ -0,0 +1,91 @@
+/*
+ * This registers and AccelerometerService handler and increments a counter each time
+ * the handler is called.  Pressing the down button displays the current count.
+ * After a fairly short time (typically < 3 minutes) it stops updating.
+ */
+
+#include <pebble.h>
+
+static Window *window;
+static TextLayer *text_layer;
+static uint16_t acc_count = 0;
+
+static void select_click_handler(ClickRecognizerRef recognizer, void *context) {
+  text_layer_set_text(text_layer, "Select");
+}
+
+static void up_click_handler(ClickRecognizerRef recognizer, void *context) {
+
+	static char percentText[11];
+	BatteryChargeState state = battery_state_service_peek();
+	snprintf(percentText,sizeof(percentText), "Bat: %hd%%",state.charge_percent);
+
+	text_layer_set_text(text_layer, percentText);
+}
+
+static void down_click_handler(ClickRecognizerRef recognizer, void *context) {
+	static char msg[30];
+	snprintf(msg, sizeof(msg), "Acc count %u", acc_count);
+	text_layer_set_text(text_layer, msg);
+}
+
+static void click_config_provider(void *context) {
+  window_single_click_subscribe(BUTTON_ID_SELECT, select_click_handler);
+  window_single_click_subscribe(BUTTON_ID_UP, up_click_handler);
+  window_single_click_subscribe(BUTTON_ID_DOWN, down_click_handler);
+}
+
+static void window_load(Window *window) {
+  Layer *window_layer = window_get_root_layer(window);
+  GRect bounds = layer_get_bounds(window_layer);
+
+  text_layer = text_layer_create((GRect) { .origin = { 0, 72 }, .size = { bounds.size.w, 20 } });
+  text_layer_set_text(text_layer, "Press Down for Count");
+  text_layer_set_text_alignment(text_layer, GTextAlignmentCenter);
+  layer_add_child(window_layer, text_layer_get_layer(text_layer));
+}
+
+static void window_unload(Window *window) {
+  text_layer_destroy(text_layer);
+}
+
+static void accelHandle(AccelData *data, uint32_t num_samples) {
+	acc_count++;
+}
+
+static void acc_start()
+{
+	accel_data_service_subscribe(25, accelHandle);
+	accel_service_set_sampling_rate(ACCEL_SAMPLING_100HZ);
+}
+
+static void acc_stop()
+{
+	accel_data_service_unsubscribe();
+}
+
+static void init(void) {
+  window = window_create();
+  window_set_click_config_provider(window, click_config_provider);
+  window_set_window_handlers(window, (WindowHandlers) {
+    .load = window_load,
+    .unload = window_unload,
+  });
+
+  acc_start();
+
+  const bool animated = true;
+  window_stack_push(window, animated);
+}
+
+static void deinit(void) {
+  window_destroy(window);
+  acc_stop();
+}
+
+int main(void) {
+  init();
+
+  app_event_loop();
+  deinit();
+}
